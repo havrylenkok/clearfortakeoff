d38172a9b37d9462713e53ffeba2be50456c1374
Volodymyr Mihav <mihaw.wolodymyr@gmail.com>
2016-04-18 18:47:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/metar-test', metarTest);
+app.use('/metar', metarTest);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
