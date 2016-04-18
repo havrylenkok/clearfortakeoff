9f2d44037dddcf2b70a4b8ff9213abe5e45c3f09
robben1234 <c.gavrilenko.96@gmail.com>
2016-04-18 19:51:04 +0300

    sync parseMetar

--- routes/metar-test.js
+++ routes/metar-test.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var metar = require('./../service/metarParser');
 
 
 router.get('/', function(req, res, next) {
--- service/metarParser.js
+++ service/metarParser.js
@@ -2,25 +2,15 @@
  * Created by robben1 on 4/14/16.
  */
 
-var request = require('request');
-var EventEmitter = require("events").EventEmitter;
-var jsdom = require('jsdom');
-var contents = new EventEmitter();
+var request = require('sync-request');
 
 var parseMetar = function (airport) {
     var adress1 = "http://www.aviationweather.gov/adds/metars/?station_ids=";
     var adress2 = "&std_trans=standard&chk_metars=on&hoursStr=most+recent+only&chk_tafs=on&submitmet=Submit";
 
+    var contents = request('GET', adress1 + airport + adress2);
 
-    request(adress1 + airport + adress2, function (error, res, body) {
-        contents.data = body;
-        contents.emit('update');
-    });
-    contents.on('update', function () {
-        console.log(contents.data);
-    });
-    
-    return contents.data;
+    return contents.getBody();
 };
 
 
