9a3dd34baa9ff7ffa754560f3bd20b46beebb1b9
robben1234 <c.gavrilenko.96@gmail.com>
2016-04-18 18:52:52 +0300

    route for metar-test

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/metar', metarTest);
+app.use('/metar-test', metarTest);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
