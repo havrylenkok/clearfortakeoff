a327a144d9c76cab86d302e3c7ec2921819d020b
Volodymyr Mihav <mihaw.wolodymyr@gmail.com>
2016-04-18 18:37:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var metar-test = require('./routes/metar-test');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/metar-test', metar-test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
