7701330d5840dc9f24751fdfac91a7f7f58ca009
robben1234 <c.gavrilenko.96@gmail.com>
2016-04-21 19:22:03 +0300

    metar parser, factory
    started prob calc

--- controllers/testController.js
+++ controllers/testController.js
@@ -3,7 +3,17 @@
  * URI: /test
  * */
 var metar = require('./../service/metarParser');
+var prob = require('./../service/metarToDelay');
 
 exports.main = function(req, res, next) {
-    res.render('test', { title: 'Clear for take off', metar: metar('UKKK') });
+    var metarobj = metar.parse('UUDD', 0);
+    // console.log(metarobj.wind.speed);
+
+
+    res.render('test', {
+        title: 'Clear for take off',
+        metar: metarobj,
+        pureMetar: metar.pureMetar('UUDD'),
+        prob: prob(metarobj, 0)
+    });
 };
--- service/metarParser.js
+++ service/metarParser.js
@@ -9,18 +9,19 @@ var window = document.defaultView;
 var $ = require('jquery')(window);
 var metarjs = require('metar-js');
 
-var parseMetar = function (airport) {
-    
-    //console.log(humanifyMetar(getMetar(airport))[0]);
-    var data = humanifyMetar(getMetar(airport));
-    var result = "";
+var parseMetar = function (airport, hoursFromNow) {
 
-    for(var i=0; i<data.length; i++) result += JSON.stringify(data[i]) + "\n";
-    return result
+    //console.log(humanifyMetar(getMetar(airport))[0]);
+    return data = humanifyMetar(getMetar(airport, hoursFromNow));
+    // var result = "";
+
+    // for (var i = 0; i < data.length; i++) result += JSON.stringify(data[i]) + "\n";
+    // console.log("parseMetar result: " + result);
+    // return result;
 };
 
-var getMetar = function (airport) {
-    
+var getMetar = function (airport, hoursFromNow) {
+
     var adress1 = "http://www.aviationweather.gov/adds/metars/?station_ids=";
     var adress2 = "&std_trans=standard&chk_metars=on&hoursStr=most+recent+only&chk_tafs=on&submitmet=Submit";
 
@@ -32,50 +33,96 @@ var getMetar = function (airport) {
     var meteoData = $(html).find("font");
     var metar = $(meteoData.get(0)).text();
     var taf = $(meteoData.get(1)).text();
-    
-    
-    return {metar: metar, taf:  taf, airport : airport};
-    
-    // TODO: get metar + taf, split it by \n
-    // TODO: у каждого рядка taf заменить идентификаторы времени на код аэропорта, для корректной работы. Забирать
-    // TODO: из массива только то, что необходимо. При этом будет обычный json с погодой, на основе которой можно выдать
-    // TODO: вероятность
+
+
+    return {metar: metar, taf: taf, airport: airport, hours: hoursFromNow};
 
 };
 
 var humanifyMetar = function (data) {
-    
+
+    var curTime = new Date().getHours();
+    var neededTime = curTime + data.hoursFromNow;
     var tafParsed = data.taf.split('\n');
 
+    // delete TAF
     tafParsed[0] = tafParsed[0].slice(3);
+    // console.log(tafParsed);
 
-    var time = tafParsed[0].match(/\d+Z/);
-    tafParsed[1] = tafParsed[1].replace(/\w+\b/, data.airport + " " + time);
-    
-    var result = [];
-    
-    result[0] = metarToJson(data.metar);
-    result[1] = metarToJson(tafParsed[0]);
-    result[2] = metarToJson(tafParsed[1]);
-    
-    console.log("Metar: \n" + data.metar);
-    console.log("Taf: \n" + data.taf);
+    var issueTime = tafParsed[0].match(/\d+Z/);
+    var firstSymbols = data.airport + " " + issueTime;
+    // console.log("ISSUE TIME: " + issueTime + "\n");
+    // var timeRangeForecast = tafParsed[0].match(/[\d]+\/+[\d]+/);
+    // console.log("TIME: " + timeRangeForecast);
 
-    console.log("Taf parsed: \n" + tafParsed);
+    var result;
+    var time;
+    var range;
 
-    console.log("Json metar: \n");
-    console.log(metarToJson(data.metar));
-    console.log("Json tafParsed[0] \n");
-    console.log(metarToJson(tafParsed[0]));
+    if (data.hoursFromNow < 2) {
+        result = metarToJs(data.metar);
+    } else {
+        result = metarToJs(tafParsed[0]);
+        for (var i = 1; i < tafParsed.length; i++) {
+            if (tafParsed[i].match(/FM/)) {
+                time = tafParsed[i].match(/FM [\d]+/);
+                time = time.substring(time.length - 2, time.length);
+                if (time <= neededTime) result = tafToJs(tafParsed[i], firstSymbols);
+            } else if (tafParsed[i].match(/becmg/) || tafParsed[i].match(/TEMPO/)) {
+                range = timeRange(tafParsed[i]);
+                if (neededTime < range.end && neededTime > range.start) result = tafToJs(tafParsed[i], firstSymbols);
+            } else if (tafParsed[i].match(/PROB/)) {
+                time = tafParsed[i].match(/PROB\d\d [\d]+/);
+                range = {start: time.substring(9, 11), end: time.substring(time.length - 2, time.length)};
+                if (neededTime < range.end && neededTime > range.start) result = tafToJs(tafParsed[i], firstSymbols);
+            }
+        }
 
-    console.log("Json tafParsed[1] \n");
-    console.log(metarToJson(tafParsed[1]));
-    
+    }
+
+
+    console.log("humanify result: " + result);
     return result;
-};
 
-var metarToJson = function (metar) {
+
+// result[0] = metarToJs(data.metar);
+// result[1] = metarToJs(tafParsed[0]);
+// result[2] = metarToJs(tafParsed[1]);
+
+    // console.log("Metar: \n" + data.metar);
+    // console.log("Taf: \n" + data.taf);
+    //
+    // console.log("Taf parsed: \n" + tafParsed);
+    //
+    // console.log("Json metar: \n");
+    // console.log(metarToJs(data.metar));
+    // console.log("Json tafParsed[0] \n");
+    // console.log(metarToJs(tafParsed[0]));
+    //
+    // console.log("Json tafParsed[1] \n");
+    // console.log(metarToJs(tafParsed[1]));
+
+    // return result;
+}
+
+
+var metarToJs = function (metar) {
     return metarjs.parseMetar(metar);
 };
 
-module.exports = parseMetar;
+var timeRange = function (string) {
+    var time = ''+ string.match(/.+ [\d]+\/[\d]+/);
+    var timeEnd = time.substring(time.length - 2, time.length);
+    var timeStart = time.substring(6, 8);
+    return {start: timeStart, end: timeEnd};
+};
+
+var tafToJs = function(tafString, firstSymbols) {
+    tafString = tafString.replace(/\w+\b/, firstSymbols);
+    return metarToJs(tafString);
+};
+
+module.exports = {
+    parse: parseMetar,
+    pureMetar: getMetar
+};
