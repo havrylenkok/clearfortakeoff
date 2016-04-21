6d8b1bb958a20092d0179d8e9e574e17e40108bd
robben1234 <c.gavrilenko.96@gmail.com>
2016-04-21 19:40:18 +0300

    dbinfo

--- controllers/testController.js
+++ controllers/testController.js
@@ -14,6 +14,11 @@ exports.main = function(req, res, next) {
         title: 'Clear for take off',
         metar: metarobj,
         pureMetar: metar.pureMetar('UUDD'),
-        prob: prob(metarobj, 0)
+        prob: prob(metarobj, 0),
+        host: process.env.OPENSHIFT_MYSQL_DB_HOST,
+        port: process.env.OPENSHIFT_MYSQL_DB_PORT,
+        name: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+        pass: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+        url: process.env.OPENSHIFT_MYSQL_DB_URL
     });
 };
