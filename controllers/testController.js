d1c2bac21cc263246c2d49aec628b80e20368ef8
Victor Turturika <v.turturika@gmail.com>
2016-04-23 00:25:56 +0300

    True connection to db

--- controllers/testController.js
+++ controllers/testController.js
@@ -5,8 +5,7 @@
 var metar = require('./../service/metarParser');
 var prob = require('./../service/metarToDelay');
 var metarApi = require('./../service/metarApi');
-var dbconnector = require('./../dbconnector');
-
+var db = require('./../dbconnector');
 
 exports.main = function(req, res, next) {
     var metarobj = metar.parse('UKKK', 0);
@@ -16,7 +15,14 @@ exports.main = function(req, res, next) {
     console.log("PROBABILITY: "); console.log(probability);
     var fakeView = metarApi('UKKK', 0, 'UUDD', 10);
     
-
+    db.testConnection(function(err, data){
+       if(err) {
+           console.log('Database error : ' + err);
+       }
+       else {
+           console.log('Data from db : ' + JSON.stringify(data));
+       }
+    });
 
     res.render('test', {
         title: 'Clear for take off',
@@ -28,7 +34,6 @@ exports.main = function(req, res, next) {
         name: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
         pass: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
         url: process.env.OPENSHIFT_MYSQL_DB_URL,
-        db: dbconnector(),
         fake: fakeView
     });
 };
--- dbconnector/index.js
+++ dbconnector/index.js
@@ -1,36 +1,29 @@
 var mysql = require('mysql');
-var metarParser()
 
-var text = "connection failed";
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
     host     : process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost',
     user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'victor',
     password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD || 'qwerty',
     port     : process.env.OPENSHIFT_MYSQL_DB_PORT || 3306,
-    database : 'Clearfortakeoff'
+    database : 'Clearfortakeoff',
+    connectionLimit : 10
 });
 
-connection.connect();
+var testConnection = function(callback) {
 
-connection.query('SELECT id, icao FROM airport', function(err, rows, fields) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        for(var i = 0; i < rows.length; i++) {
-
-            console.log(rows[i]);
+    pool.query({sql: "SELECT * FROM airport WHERE icao = ?", values:['UKKK']}, function (err, rows) {
+        if(err) {
+            console.log("Catch error: " + err);
+            callback(err, null);
         }
-
-    }
-});
-
-connection.end();
-
-var dbresult = function () {
-    return text;
+        else {
+            console.log("Successfully connected : " + rows.id);
+            callback(null, rows[0]);
+        }
+    })
 };
 
-module.exports = dbresult;
+
+module.exports = {testConnection : testConnection};
 
 
