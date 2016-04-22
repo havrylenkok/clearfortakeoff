9ff9153873ae5bc7c39953c83f25dc88ae3f38a3
Victor Turturika <v.turturika@gmail.com>
2016-04-22 12:48:28 +0300

    test db connection 2

--- controllers/testController.js
+++ controllers/testController.js
@@ -5,6 +5,28 @@
 var metar = require('./../service/metarParser');
 var prob = require('./../service/metarToDelay');
 
+var mysql = require('mysql');
+var text = "connection failed";
+var connection = mysql.createConnection({
+    host     : process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost',
+    user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'victor',
+    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD || 'qwerty',
+    port     : process.env.OPENSHIFT_MYSQL_DB_PORT || 3306
+});
+
+connection.connect();
+
+connection.query('SELECT 1', function(err, rows, fields) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Connected');
+    text = "connected";
+});
+
+connection.end();
+
+
 exports.main = function(req, res, next) {
     var metarobj = metar.parse('UUDD', 0);
     // console.log(metarobj.wind.speed);
@@ -19,6 +41,7 @@ exports.main = function(req, res, next) {
         port: process.env.OPENSHIFT_MYSQL_DB_PORT,
         name: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
         pass: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
-        url: process.env.OPENSHIFT_MYSQL_DB_URL
+        url: process.env.OPENSHIFT_MYSQL_DB_URL,
+        db: text
     });
 };
