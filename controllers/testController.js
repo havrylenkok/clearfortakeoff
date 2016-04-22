65f4c8900e650e56098b251b740eaf9dc8987c15
robben1234 <c.gavrilenko.96@gmail.com>
2016-04-22 12:35:40 +0300

    Revert "Merge branch 'database'"
    This reverts commit ced8176ec6ff8f91891ecbcd89c577f69488a376, reversing
    changes made to 6d8b1bb958a20092d0179d8e9e574e17e40108bd.

--- controllers/testController.js
+++ controllers/testController.js
@@ -5,34 +5,13 @@
 var metar = require('./../service/metarParser');
 var prob = require('./../service/metarToDelay');
 
-
-var text = "connection failed";
-
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-    host     : process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost',
-    user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME || 'victor',
-    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD|| 'qwerty',
-    port :  process.env.OPENSHIFT_MYSQL_DB_PORT || '3306'
-});
-
-connection.connect();
-
-connection.query('SELECT 1', function(err, rows, fields) {
-    if (err) {console.log(err)}
-    else {
-        console.log('Connected');
-        text = "Connected";
-    }
-});
-
-connection.end();
-
-
 exports.main = function(req, res, next) {
     var metarobj = metar.parse('UUDD', 0);
+    // console.log(metarobj.wind.speed);
 
-    res.render('test', { title: 'Clear for take off', weather: metar('UKKK'), db : text,
+
+    res.render('test', {
+        title: 'Clear for take off',
         metar: metarobj,
         pureMetar: metar.pureMetar('UUDD'),
         prob: prob(metarobj, 0),
@@ -41,10 +20,5 @@ exports.main = function(req, res, next) {
         name: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
         pass: process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
         url: process.env.OPENSHIFT_MYSQL_DB_URL
-    }
-
-
-    );
-
-
+    });
 };
