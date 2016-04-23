d933de098fd76251ec3a65a9f7942fb0a1f5e93a
robben1234 <c.gavrilenko.96@gmail.com>
2016-04-23 15:18:52 +0300

    null checks, maxProb 99%

--- service/metarToDelay.js
+++ service/metarToDelay.js
@@ -8,7 +8,7 @@ var countProbability = function (jsMetar
     var edges = jsMetar.edge;
     if (edges != null) {
         if (edges == 'good') return {probability: 1, delay: 5};
-        if (edges == 'bad') return {probability: 100, delay: 720};
+        if (edges == 'bad') return {probability: 99, delay: 720};
     }
 
     jsMetar = jsMetar.result;
@@ -68,7 +68,7 @@ var countProbability = function (jsMetar
 
     }
 
-    if(probabilityOfDelay > 100) probabilityOfDelay = 100;
+    if(probabilityOfDelay > 99) probabilityOfDelay = 99;
     console.log("Prob" + probabilityOfDelay + " time: " + delayInMins);
     return {probability: probabilityOfDelay, delay: delayInMins};
 };
