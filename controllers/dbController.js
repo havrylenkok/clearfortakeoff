9b687944c0e4152cfc1368f3815573e4d5c8dfc0
Victor Turturika <v.turturika@gmail.com>
2016-04-23 12:34:01 +0300

    try connect to remote server

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,28 +1,26 @@
 
 var db = require('./../dbconnector');
 
-var showDataFromDb = function(err, data) {
-    if(err) {
-        console.log('Database error : ' + err);
-    }
-    else {
-        console.log('Data from db : ' + JSON.stringify(data));
-    }
-};
-
-db.testConnection(showDataFromDb);
-
-db.getIcaoOrIata('UKKK', showDataFromDb);
-
-db.getIcaoOrIata('IEV', showDataFromDb);
-
-db.updateTop({prob:3, delay:3, icao:'UKKG'});
-
-db.getTop({limit:3}, showDataFromDb);
-
-db.getNearestAirport({userLat:48.49, userLng:32.21, limit:5}, showDataFromDb);
-
 exports.main = function (req, res, next) {
+
+    var showDataFromDb = function(err, data) {
+        if(err) {
+            console.log('Database error : ' + err);
+        }
+        else {
+            console.log('Data from db : ' + JSON.stringify(data));
+        }
+    };
+
+    db.testConnection(showDataFromDb);
+
+    db.getIcaoOrIata('UKKK', showDataFromDb);
+
+    db.getIcaoOrIata('IEV', showDataFromDb);
+
+    db.updateTop({prob:3, delay:3, icao:'UKKG'});
+
+    db.getTop({limit:3}, showDataFromDb);
 
     db.getNearestAirport({userLat:48.49, userLng:32.21, limit:5}, function(err, data) {
         if(err) {
