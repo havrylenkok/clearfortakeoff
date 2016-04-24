0401407ea2bffcaa76cb3ea9a735d52f954b4e5f
robben1234 <c.gavrilenko.96@gmail.com>
2016-04-24 05:50:01 +0300

    fakestart

--- controllers/fakeViewController.js
+++ controllers/fakeViewController.js
@@ -10,48 +10,16 @@ var prob = require('./../service/metarToDelay');
 var metarApi = require('./../service/metarApi');
 var parseFlight = require('./../service/parseFlight');
 var city = require('./../service/parseCityInfo');
+var db = require('./../dbconnector/index');
 
-var results = {};
-
-var callbackCity = function (res) {
-    // console.log(res);
-    addToResults('city', res);
-};
-
-var addToResults = function (name, res) {
-    results[name] = res;
-    console.log(results[name]);
-    renderPage();
-};
-
-var renderPage = function () {
-    if(results.length > 1) {
-        renderer.render('fakeView', {
-            title: 'blah',
-            fake: results['city'],
-            metar: results['metar']
-        });
-    }     
-};
 
 exports.main = function(req, res, next) {
-    var metarobj = metar.parse('UKKK', 0);
-    // console.log("PARSED METAR: "); console.log(metarobj);
-    var probability = prob(metarobj, 0, 2);
-    // console.log(metarobj.wind.speed);
-    // console.log("PROBABILITY: "); console.log(probability);
-    //
-    //var request = require("request")
-    //
-
-
-    metarApi('UKKK', 0, 2, 0, 'UUDD', 10, 2, 0, callbackMetar);
-    city('Kiev', callbackCity);
-    var jfkMetarPure = metar.parse('KJFK', 0);
-    //console.log("KJFK PURE METAR");
-    //console.log(jfkMetarPure);
-    parseFlight();
-
-    // console.log("TYPE OF: " + callbackCity);
-    // city('Kiev', callbackCity);
+    
+    db.getIcaoOrIata('UKKK', function (err, res) {
+        if(!err) {
+            db.getIcaoOrIata('KJFK', function (err2, res2) {
+                metarApi(res, 0, )
+            })
+        }
+    })
 };
